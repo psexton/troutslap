247d872a064fc13305dae9fe6e5cc24a78db6caf
Paul Sexton <github@psexton.net>
2021-04-25 21:41:39 -0500

    Reformat

--- troutslap.py
+++ troutslap.py
@@ -4,11 +4,13 @@ from flask import abort, Flask, jsonify, request
 
 app = Flask(__name__)
 
+
 def is_request_valid(request):
     is_token_valid = request.form['token'] == os.environ['SLACK_VERIFICATION_TOKEN']
     is_team_id_valid = request.form['team_id'] == os.environ['SLACK_TEAM_ID']
 
     return is_token_valid and is_team_id_valid
+
 
 @app.route('/hook', methods=['POST'])
 def hello_there():
