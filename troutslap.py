26b660b5511f08f1eeae34ce8f7b22b6efa8a0f6
Paul Sexton <github@psexton.net>
2021-06-16 21:34:21 -0500

    Add AGPL preamble

--- troutslap.py
+++ troutslap.py
@@ -1,3 +1,21 @@
+# <editor-fold desc="AGPLv3 preamble">
+# troutslap
+# Copyright (C) 2021  Paul Sexton
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# </editor-fold>
+
 import boto3
 import hashlib
 import hmac
